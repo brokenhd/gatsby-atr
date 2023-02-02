84af5ee5ccac105208f2c5334dc15adc82798018
brokenhd <michaelastarks@gmail.com>
2023-02-02 15:45:06 -0700

    step 2

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+
+const AboutPage = () => {
+  return (
+    <Layout pageTitle="About">
+      <p>This is the second page.</p>
+    </Layout>
+  )
+}
+
+export const Head = () => <title>About This</title>
+
+export default AboutPage
