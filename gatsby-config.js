34da48799bbeeb9652866613e85a3c46a832a82e
Michael Starks <michael.starks@bkwld.com>
2023-02-02 15:10:54 -0700

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `After The Rain`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
