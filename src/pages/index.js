8b3554de5b065e8301264e87d2582fb755c853b2
brokenhd <michaelastarks@gmail.com>
2023-02-02 18:05:04 -0700

    step 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,20 @@
 import * as React from "react"
 import Layout from '../components/layout'
+import { StaticImage } from "gatsby-plugin-image"
+import Seo from '../components/seo'
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>This is what happens.</p>
+      <StaticImage
+        alt="Something"
+        src="../images/dog.webp"
+      />
     </Layout>
   )
 }
 
-export const Head = () => <title>Home Page</title>
+export const Head = () => <Seo title="Home Page" />
 
 export default IndexPage
